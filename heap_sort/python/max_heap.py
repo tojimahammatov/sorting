81b75199adf26b23ec795d35713a6ea40358fbc1
tojimahammatov <tojimahammatov@gmail.com>
2021-01-27 19:23:39 +0900

    heapify impl.

--- heap_sort/python/max_heap.py
+++ heap_sort/python/max_heap.py
@@ -1,4 +1,4 @@
-
+HEAP_SIZE = 10
 
 def left_child(index):
     """
@@ -17,3 +17,24 @@ def parent(index):
     returns index of a parent in the list
     """
     return index//2
+
+def max_heapify(A, index):
+    """
+    applies (max) heapify operation to the given list starting from the index
+    """
+    global HEAP_SIZE
+    left = left_child(index)
+    right = right_child(index)
+
+    largest = index
+
+    if left<=HEAP_SIZE and A[index]<A[left]:
+        largest = left
+    
+    if right<=HEAP_SIZE and A[largest]<A[right]:
+        largest = right
+
+    if largest != index:
+        A[index], A[largest] = A[largest], A[index]
+        max_heapify(A, largest)
+
