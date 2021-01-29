5ecd79c77e1d34947a242ec7d3d76879d4078cfb
tojimahammatov <tojimahammatov@gmail.com>
2021-01-29 20:31:00 +0900

    heapsort, tested example

--- heap_sort/c++/max_heap.cpp
+++ heap_sort/c++/max_heap.cpp
@@ -20,11 +20,11 @@ void max_heapify(int A[], int index){
     int left = left_child(index);
     int right = right_child(index);
     int largest = index;
-    if(left<=HEAP_SIZE && A[left]<A[index]){
-        largest = index;
+    if(left<=HEAP_SIZE && A[index]<A[left]){
+        largest = left;
     }
-    if(right<=HEAP_SIZE && A[right]<A[largest]){
-        largest = index;
+    if(right<=HEAP_SIZE && A[largest]<A[right]){
+        largest = right;
     }
     if (largest != index){
         swap(A[largest], A[index]);
@@ -38,6 +38,20 @@ void build_max_heap(int A[]){
     }
 }
 
-int main(){
+void heapsort(int A[]){
+    for (int i = 0; i < 10; i++){
+        swap(A[HEAP_SIZE], A[1]);
+        HEAP_SIZE--;
+        max_heapify(A, 1);
+    }
+}
 
+int main(){
+    int A[] = {-999, 4, 9, 1, 2, 0, 5, 7, 3, 8, 6};
+    build_max_heap(A);
+    heapsort(A);
+    for (size_t i = 0; i < 11; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
 }
