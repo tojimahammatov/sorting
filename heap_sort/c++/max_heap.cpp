43e1b89fa2d820accd68cadd95f084df5ec23a91
tojimahammatov <tojimahammatov@gmail.com>
2021-01-29 20:00:17 +0900

    right_child impl., parent, max_heapify, build_max_heap partial

--- heap_sort/c++/max_heap.cpp
+++ heap_sort/c++/max_heap.cpp
@@ -8,7 +8,26 @@ int left_child(int index){
     return (index<<1);
 }
 
+int right_child(int index){
+    return (index<<1)+1;
+}
 
+int parent(int index){
+    return index>>1;
+}
+
+void max_heapify(int A[], int index){
+    int left = left_child(index);
+    int right = right_child(index);
+
+}
+
+void build_max_heap(int A[]){
+    for (int i = HEAP_SIZE/2; i > 0; i--){
+        
+    }
+    
+}
 
 int main(){
 
