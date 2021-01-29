0ef862692171fe12d8b6cef30decfcddb49e8b60
tojimahammatov <tojimahammatov@gmail.com>
2021-01-29 20:03:18 +0900

    finish max_heapify and build_max_heap

--- heap_sort/c++/max_heap.cpp
+++ heap_sort/c++/max_heap.cpp
@@ -19,14 +19,23 @@ int parent(int index){
 void max_heapify(int A[], int index){
     int left = left_child(index);
     int right = right_child(index);
-
+    int largest = index;
+    if(left<=HEAP_SIZE && A[left]<A[index]){
+        largest = index;
+    }
+    if(right<=HEAP_SIZE && A[right]<A[largest]){
+        largest = index;
+    }
+    if (largest != index){
+        swap(A[largest], A[index]);
+        max_heapify(A, largest);
+    }
 }
 
 void build_max_heap(int A[]){
     for (int i = HEAP_SIZE/2; i > 0; i--){
-        
+        max_heapify(A, i);
     }
-    
 }
 
 int main(){
